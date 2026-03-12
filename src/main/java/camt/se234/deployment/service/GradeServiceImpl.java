77be3d9605c12b7568a1d511f9dd535ad960aa6a
672115045 Virawit Kongthong <guykongthongcmu@gmail.com>
2026-03-13 01:37:16 +0700

    Fix local backend port and Maven wrapper setup

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -1,20 +1,19 @@
 package camt.se234.deployment.service;
 
-import camt.se234.deployment.entity.Grade;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GradeServiceImpl implements GradeService {
     @Override
     public String getGrade(double score) {
-        if (score > 79.5) {
+        if (score >= 79.0) {
             return "A";
         }
-        else if (score > 74.5){
+        else if (score >= 74.5){
             return "B";
-        }else if (score > 59.5){
+        }else if (score >= 59.2){
             return "C";
-        }else if (score > 32.5) {
+        }else if (score > 33.0) {
             return "D";
         } else
             return "F";
